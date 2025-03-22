0af4ecec5ed76a4c2c244c3f5c0ec495a4a11deb
Wyl-ASG <yiluan.li@gmail.com>
2025-03-22 15:57:53 +0800

    Update Emotion.vue

--- my-vue-app/src/pages/Emotion.vue
+++ my-vue-app/src/pages/Emotion.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="text-center">
-      <h1 class="display-4">Contact Page</h1>
-      <p class="lead">Get in touch with us.</p>
+      <h1 class="display-4">Chatbot</h1>
     </div>
   </template>
