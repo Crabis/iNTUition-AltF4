ffb0f1aae4af5ba5c73fd44a091e943eed3ac259
Wyl-ASG <yiluan.li@gmail.com>
2025-03-22 14:24:13 +0800

    Update About.vue

--- my-vue-app/src/pages/About.vue
+++ my-vue-app/src/pages/About.vue
@@ -1,7 +1,22 @@
 <template>
     <div class="text-center">
       <h1 class="display-4">About Page</h1>
-      <p class="lead">Learn more about us here.</p>
+      <p class="lead">{{ message }}</p>
+      <button class="btn btn-primary" @click="changeMessage">Change Message</button>
     </div>
   </template>
   
+  <script>
+  export default {
+    data() {
+      return {
+        message: "Learn more about us here."
+      };
+    },
+    methods: {
+      changeMessage() {
+        this.message = "Thank you for visiting the About page!";
+      }
+    }
+  };
+  </script>
