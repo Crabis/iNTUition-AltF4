838cc8656c5f180ea35eb33116cc75d92543f86d
Wyl-ASG <yiluan.li@gmail.com>
2025-03-22 14:11:13 +0800

    frontend

---
+++ my-vue-app/src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../pages/Home.vue';
+import About from '../pages/About.vue';
+import Contact from '../pages/Contact.vue';
+import NotFound from '../pages/NotFound.vue';
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+    { path: '/contact', component: Contact },
+    { path: '/:pathMatch(.*)*', component: NotFound } // Catch-all for 404
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
